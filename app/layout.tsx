97e6c55f90dc41478179eca8bea33e29e3b5bee0
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-07-13 22:17:20 +0530

    Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+import { ClerkProvider } from "@clerk/nextjs"
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,17 +34,19 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <QueryProvider>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
-        </ThemeProvider>
-        </QueryProvider>
-        </body>
+        <ClerkProvider>
+          <QueryProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </QueryProvider>
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
